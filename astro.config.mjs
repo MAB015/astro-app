79ea417bb4be752a1cd96de290511096a479ec70
Miguel Ángel <MAB015@HOTMAIL.COM>
2024-01-05 00:17:36 -0500

    Managing states with preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), preact()],
+  output: 'hybrid',
+  adapter: vercel()
 });
